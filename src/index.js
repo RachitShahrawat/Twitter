96f07eff37df058a82ff4c596e3b2f303596b45b
RachitShahrawat <rachitshahrawat@gmail.com>
2025-02-18 03:22:31 +0530

    setup the project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const connect=require('./config/database');
+
+const app=express();
+
+const TweetRepository=require('./repository/tweet-repository');
+const Comment=require('./models/comment');
+
+app.listen(3000, async ()=>{
+    console.log('Server started');
+    await connect();
+    console.log('mongo db connected');
+
+    // const tweet=await Tweet.create({
+    //     content:'third tweet',
+    //     // userEmail:'a@b.com'
+    // });
+    // const tweets=await Tweet.find();
+    // const tweet=await Tweet.findById('67b259d2b7861e3f92b11e49');
+    // tweet.userEmail='b@c.com';
+    // await tweet.save();
+
+    const tweetRepo=new TweetRepository;
+
+    const tweet=await tweetRepo.create({content:'tweet with comment schema'});
+    console.log(tweet);
+    const comment=await Comment.create({content:'new comment'});
+    tweet.comments.push(comment);
+    await tweet.save();
+    console.log(tweet);
+})
