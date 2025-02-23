c18d31c56968ba395afb79bd19b83b45984679b8
RachitShahrawat <rachitshahrawat@gmail.com>
2025-02-23 14:49:25 +0530

    added v1 for tweet and hashtag model

--- src/index.js
+++ src/index.js
@@ -3,31 +3,10 @@ const connect=require('./config/database');
 
 const app=express();
 
-const TweetRepository=require('./repository/tweet-repository');
-const Comment=require('./models/comment');
-
 app.listen(3000, async ()=>{
     console.log('Server started');
     await connect();
     console.log('mongo db connected');
 
-    // const tweet=await Tweet.create({
-    //     content:'third tweet',
-    //     // userEmail:'a@b.com'
-    // });
-    // const tweets=await Tweet.find();
-    // const tweet=await Tweet.findById('67b259d2b7861e3f92b11e49');
-    // tweet.userEmail='b@c.com';
-    // await tweet.save();
-
-    const tweetRepo=new TweetRepository;
-    await tweetRepo.create({content:'With hooks'});
-
-    // const tweet=await tweetRepo.create({content:'tweet with comment schema'});
-    // console.log(tweet);
-    // const comment=await Comment.create({content:'new comment'});
-    // tweet.comments.push(comment);
-    // const tweet=await tweetRepo.getAll(2,4)
-    // await tweet.save();
-    // console.log(tweet[0].contentWithEmail);
+   
 })
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,29 +4,20 @@ const tweetSchema=new mongoose.Schema({
     content:{
         type:String,
         required:true,
+        max:[250,'Tweet cannot be more than 250 characters']
     },
-    userEmail:{
-        type:String
-    },
-    comments:[
+    hashtags:[
         {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Comment'
-
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Hashtag'
         }
     ]
+    
 },{
     timestamps:true
 });
 
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return `${this.content}\nCreated by:${this.userEmail}`
-});
 
-tweetSchema.pre('save',function(next){
-    console.log('Inside a hook');
-    next();
-})
 
 const Tweet=mongoose.model('Tweet',tweetSchema);
 
